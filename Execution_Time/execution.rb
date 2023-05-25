7d0a69319e3104d706f01b7cf0a33f2f70f1086f
Jordy <jordycorporan95@gmail.com>
2023-05-25 11:52:38 -0400

    Solved my min

---
+++ Execution_Time/execution.rb
@@ -0,0 +1,42 @@
+def my_min(array)
+
+#  n = array.length - 1
+
+
+ sorted = false
+
+  until sorted
+    sorted = true
+
+
+    (0...array.length - 1).each do |i|
+      if (array[i] > array[i + 1])
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = false
+      end
+    end
+  end
+  array.first
+
+end
+
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min(list)
+
+def my_min(array)
+  current = 0
+  array.each do |ele|
+    if ele < current
+      current = ele
+    end
+  end
+  return current
+
+
+end
+
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+
+p my_min(list)
