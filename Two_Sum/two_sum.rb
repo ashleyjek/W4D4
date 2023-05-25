7a135cc4c2ad08d8a761df99065e1cc8a83396f8
Jordy <jordycorporan95@gmail.com>
2023-05-25 16:45:53 -0400

    Solved okay_two_sum?

--- Two_Sum/two_sum.rb
+++ Two_Sum/two_sum.rb
@@ -15,16 +15,29 @@ arr = [0, 1, 5, 7]
 p bad_two_sum?(arr, 6) # => should be true
 p bad_two_sum?(arr, 10) # => should be false
 
+# require "byebug"
+
 
 def okay_two_sum?(arr,target)
   sorted = arr.sort
 
+  # debugger
 
-  left = 0
-  right = arr.size
-
-  sorted.each
-
-
+  until sorted.empty?
+    left = sorted.first
+    right = sorted.last
+    if left + right == target
+      return true
+    elsif left + right > target
+      sorted.pop
+    else
+      sorted.shift
+    end
+  end
+  false
 
 end
+
+arr = [0, 1, 5, 7]
+
+p okay_two_sum?(arr,8)
