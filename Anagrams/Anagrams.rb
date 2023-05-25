fff7698ad05d318e9810f381f4bded06f9de7d48
ashley <ashleykimca@gmail.com>
2023-05-25 15:35:09 -0400

    Finished anagrams.rb

--- Anagrams/Anagrams.rb
+++ Anagrams/Anagrams.rb
@@ -1,4 +1,85 @@
-def anagram?(str1,str2)
+def first_anagram?(str1,str2) # => O(n!)
+  arr1 = str1.split("")
+  arr2 = str2.split("")
+
+  if arr1.length != arr2.length
+    return false
+  end
+
+  if arr1.permutation(str2.length).include?(arr2)
+    return true
+  end
+  false
+
+end
+
+# p first_anagram?("potato","cat")
+# p first_anagram?("tac","cat")
+# p first_anagram?("catt","caat")
+
+
+def second_anagram?(str1, str2) # => O(n) , #second_anagram is worst than #first_anagram
+  arr1 = str1.split("")
+  arr2 = str2.split("")
+
+  if arr1.length != arr2.length
+    return false
+  end
+
+  arr1.each do |ele|
+    if arr2.index(ele) != nil
+      arr2.delete_at(arr2.index(ele))
+    end
+  end
+
+  if arr2.length == 0
+    return true
+  end
+
+  false
+      
+end
+
+# p second_anagram?("potatockjsbadkjsbtfkjbskjssfsgkhjhgfghnjhgf","cat")
+# p second_anagram?("catt","caat")
+
+def third_anagram?(str1, str2) # O(n^2) , worse than #second_anagram?
+  alphabet = ("a".."z").to_a
+
+  sorted = false
+  until sorted
+    sorted = true
+    (0...str1.length-1).each do |i|
+      if alphabet.index(str1[i]) > alphabet.index(str1[i+1])
+        str1[i], str1[i+1] = str1[i+1], str1[i] 
+        sorted = false
+      end
+    end
+  end
+
+  sorted = false
+  until sorted
+    sorted = true
+    (0...str2.length-1).each do |i|
+      if alphabet.index(str2[i]) > alphabet.index(str2[i+1])
+        str2[i], str2[i+1] = str2[i+1], str2[i] 
+        sorted = false
+      end
+    end
+  end
+
+  if str1 == str2
+    return true
+  end
+  false
+
+end
+
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+# p third_anagram?("catto", "caato")   # => false
+
+def fourth_anagram?(str1,str2) #=> O(n)
 
   hash1 = Hash.new(0)
   hash2 = Hash.new(0)
@@ -16,25 +97,9 @@ def anagram?(str1,str2)
 end
 
 
-p anagram?("gizmo", "sally")    #=> false
-p anagram?("elvis", "lives")    #=> true
+# p fourth_anagram?("gizmo", "sally")    #=> false
+# p fourth_anagram?("elvis", "lives")    #=> true
 
-def first_anagram?(str1,str2)
-  anagrams = []
-  (0...str1.length).each do |idx1|
-    (idx1...str1.length).each do |idx2|
-      anagrams << str1[idx1..idx2]
-    end
-  end
 
-  anagrams.each do |ana|
-    if ana == str2
-      return true
-    end
-  end
-  false
 
-end
 
-p first_anagram?("potato","cat")
-p first_anagram?("potatocatkjsbdkjsbfkjbskjssfsgkhjhgfghnjhgf","catcef")
