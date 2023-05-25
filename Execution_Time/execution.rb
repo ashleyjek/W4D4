40e2f0a9c127be5dc989b10c713caa4be9bc4b68
ashley <ashleykimca@gmail.com>
2023-05-25 12:15:21 -0400

    Added largest_contiguous_subsum

--- Execution_Time/execution.rb
+++ Execution_Time/execution.rb
@@ -40,3 +40,60 @@ end
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 
 p my_min(list)
+
+def largest_contiguous_subsum(array) # <= O(n^2)
+  subs = []
+
+  (0...array.length).each do |idx1| # <= O(n^2)
+    (idx1...array.length).each do |idx2| 
+      subs << array[idx1..idx2]
+    end
+  end
+
+  largest = 0
+  subs.each do |array| #<= O(n^2)
+    if array.sum > largest
+      largest = array.sum 
+    end
+  end
+
+  largest
+
+end
+
+list = [5, 3, -7]
+p largest_contiguous_subsum(list) # => 8
+
+# possible sub-sums
+# [5]           # => 5
+# [5, 3]        # => 8 --> we want this one
+# [5, 3, -7]    # => 1
+# [3]           # => 3
+# [3, -7]       # => -4
+# [-7]          # => -7
+
+def largest_contiguous_subsum(array)
+  largest = array.first # 5
+  sum = array.first # 5
+
+  (0...array.length-1).each do |idx|
+    sum = array[idx] + array[idx+1]
+    if sum > largest
+      largest = sum
+    end
+  end
+
+  largest
+end
+
+
+list = [5, 3, -7]
+p largest_contiguous_subsum(list) # => 8
+
+# possible sub-sums
+# [5]           # => 5
+# [5, 3]        # => 8 --> we want this one
+# [5, 3, -7]    # => 1
+# [3]           # => 3
+# [3, -7]       # => -4
+# [-7]          # => -7
